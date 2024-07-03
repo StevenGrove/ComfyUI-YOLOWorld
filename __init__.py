41c6f7c2673566041145585773b52b3cb220d8d3
ronnysong <ronnysong@tencent.com>
2024-07-03 18:25:34 +0800

    add weights

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 import os
-os.system('pip uninstall -y opencv-python opencv-contrib-python opencv-python-headless')
+os.system('pip uninstall -y opencv-python opencv-contrib-python '
+          'opencv-python-headless')
 os.system('pip install opencv-python==4.7.0.72')
+os.environ['MODEL_CACHE_DIR'] = os.path.join(os.getcwd(), 'weights')
 
-from . import yolo_world
+from . import yolo_world  # noqa
 
 
 NODE_CLASS_MAPPINGS = {**yolo_world.NODE_CLASS_MAPPINGS}
 NODE_DISPLAY_NAME_MAPPINGS = {**yolo_world.NODE_DISPLAY_NAME_MAPPINGS}
-

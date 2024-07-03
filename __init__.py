c9510520a69e1d078d227e0c969e9fea74120858
ronnysong <ronnysong@tencent.com>
2024-07-03 17:11:45 +0800

    revise cv2 bug

--- __init__.py
+++ __init__.py
@@ -1,4 +1,10 @@
+import os
+os.system('pip uninstall -y opencv-python opencv-contrib-python opencv-python-headless')
+os.system('pip install opencv-python==4.7.0.72')
+
 from . import yolo_world
+
 
 NODE_CLASS_MAPPINGS = {**yolo_world.NODE_CLASS_MAPPINGS}
 NODE_DISPLAY_NAME_MAPPINGS = {**yolo_world.NODE_DISPLAY_NAME_MAPPINGS}
+
